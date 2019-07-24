a7bf4d67d2321c889b9d5ce632864e476a8ce70e
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-07-23 22:24:28 -0300

    add repository

---
+++ source/Car.Repository/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Car.Repository.Interfaces
+{
+    public interface IRepositoryBase<T>
+    {
+        T Insert(T obj);
+        void Delete(T obj);
+        List<T> GetAll();
+        List<T> Find(Func<T, bool> predicate);
+    }
+}
