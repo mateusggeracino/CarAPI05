a59424c146fd6cab85391dc6f05ff4c4d525e2f3
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-07-26 19:25:53 -0300

    add icarRepository

---
+++ source/Car.Repository/CarRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Car.Domain.Entities;
+using Car.Repository.Interfaces;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Car.Repository
+{
+    public class CarRepository : RepositoryBase<CarEntity>, ICarRepository
+    {
+        public CarRepository(ILogger<RepositoryBase<CarEntity>> logger, IConfiguration config) : base(logger, config)
+        {
+        }
+
+        public CarEntity GetByKey(Guid key)
+        {
+            var query = "select * from Car where Brand like @brand";
+            var parameters = new DynamicParameters();
+
+            return Query(query, parameters).FirstOrDefault();
+        }
+
+        public List<CarEntity> GetByBrand(string brand)
+        {
+            var query = "select * from Car where Brand like @brand";
+            var parameters = new DynamicParameters();
+            parameters.Add("@brand", brand);
+            return Query(query, parameters);
+        }
+
+    }
+}
---
+++ source/Car.Repository/Interfaces/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Car.Domain.Entities;
+
+namespace Car.Repository.Interfaces
+{
+    public interface ICarRepository : IRepositoryBase<CarEntity>
+    {
+        CarEntity GetByKey(Guid key);
+        List<CarEntity> GetByBrand(string brand);
+        List<CarEntity> GetAll();
+    }
+}
